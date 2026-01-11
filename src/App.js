cb4a96c49053feb3e25d26ae64938ba441d2f1f1
somnathr1332 <somnathr1332@gmail.com>
2026-01-11 18:24:18 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
-import ReleaseGate from './components/ReleaseGate';
-import AudioPlayer from './components/AudioPlayer';
-import Welcome from './pages/Welcome';
-import MemoryLane from './pages/MemoryLane';
-import Cake from './pages/Cake';
-import Letter from './pages/Letter';
+import React, { useEffect } from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import confetti from "canvas-confetti";
 
+import ReleaseGate from "./components/ReleaseGate";
+import AudioPlayer from "./components/AudioPlayer";
+
+import Welcome from "./pages/Welcome";
+import MemoryLane from "./pages/MemoryLane";
+import Cake from "./pages/Cake";
+import Letter from "./pages/Letter";
+
+// 🎉 Confetti trigger function
+const launchConfetti = () => {
+  confetti({
+    particleCount: 150,
+    spread: 70,
+    origin: { y: 0.6 },
+  });
+};
+
+// 🌟 Layout wrapper
 const Layout = ({ children }) => {
   return (
-    <div className="relative">
+    <div className="relative min-h-screen bg-gradient-to-b from-pink-200 to-yellow-200">
       <ReleaseGate>
         <AudioPlayer />
         {children}
@@ -18,14 +31,20 @@ const Layout = ({ children }) => {
   );
 };
 
+// 💌 App component
 const App = () => {
+  // Launch confetti on first load
+  useEffect(() => {
+    launchConfetti();
+  }, []);
+
   return (
     <HashRouter>
       <Layout>
         <Routes>
           <Route path="/" element={<Welcome />} />
           <Route path="/memory-lane" element={<MemoryLane />} />
-          <Route path="/cake" element={<Cake />} />
+          <Route path="/cake" element={<Cake launchConfetti={launchConfetti} />} />
           <Route path="/letter" element={<Letter />} />
         </Routes>
       </Layout>
